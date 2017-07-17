5491134921d8b81b57a8ccf87488ef2bed86699d
Nigel Heaney <nigel.heaney@gmail.com>
2017-07-17 11:21:38 +0100

    minor update

--- test-web/files/launch.sh
+++ test-web/files/launch.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
-#copy files to shm and launch there to speed up iops.
+#copy files to shm and use this as webroot for the performance gain.
 cp -f * /work
 cd /work
 python /apps/test-page.py > /work/index.html
